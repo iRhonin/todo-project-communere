43ecb7fbb0a347a3c04a7ba8efe1c823accbf71d
Arash Fatahzade <fatahzade@gmail.com>
2022-11-06 18:33:25 +0330

    Dockerizing

---
+++ Dockerfile
@@ -0,0 +1,44 @@
+FROM python:3.8 AS base
+
+ENV VIRTUAL_ENV=/opt/venv
+RUN pip install --upgrade pip
+RUN python3 -m venv $VIRTUAL_ENV
+
+ENV PATH="$VIRTUAL_ENV/bin:$PATH"
+COPY requirements.txt .
+RUN pip install -r requirements.txt
+
+FROM base AS base-dev
+ENV PATH="$VIRTUAL_ENV/bin:$PATH"
+COPY requirements-dev.txt .
+RUN pip install -r requirements-dev.txt
+
+FROM python:3.8-slim AS prod
+RUN apt update && apt install curl -y
+ENV VIRTUAL_ENV=/opt/venv
+COPY --from=base $VIRTUAL_ENV $VIRTUAL_ENV
+COPY . /app
+WORKDIR /app
+ENV PYTHONDONTWRITEBYTECODE 1
+ENV PYTHONUNBUFFERED 1
+ENV PATH="$VIRTUAL_ENV/bin:$PATH"
+
+CMD ["./scripts/run.sh"]
+
+FROM prod as development
+ARG USER_ID=1001
+ARG GROUP_ID=1001
+ARG USER=user
+
+RUN addgroup --gid $GROUP_ID $USER
+RUN adduser --disabled-password --gecos '' --uid $USER_ID --gid $GROUP_ID $USER
+
+RUN mkdir -p /home/user/.config/pudb ; chown -R ${USER_ID}:${GROUP_ID} /home/user/.config/pudb
+
+COPY --from=prod --chown=$USER:$USER /app /app
+
+ENV VIRTUAL_ENV=/opt/venv
+COPY --from=base-dev $VIRTUAL_ENV $VIRTUAL_ENV
+USER $USER
+ENV PATH="$VIRTUAL_ENV/bin:$PATH"
+CMD [ "/bin/bash", "./scripts/run.sh"]
--- todo/config.py
+++ todo/config.py
@@ -1,25 +1,22 @@
 import os
 from logging import DEBUG as DEBUG_LVL
 
-from dotenv import find_dotenv, load_dotenv
+from dotenv import find_dotenv
+from dotenv import load_dotenv
 
 
 class Config(object):
     ENVIRONMENT = 'local'
     DEBUG = True
     LOGLEVEL = DEBUG_LVL
-    POSTGRES_HOST = 'localhost'
+    POSTGRES_HOST = 'db'
     POSTGRES_PORT = '5432'
     POSTGRES_DB = 'todo'
     POSTGRES_TEST_DB = 'todo_test'
     POSTGRES_ADMIN_DB = 'postgres'
     POSTGRES_USER = 'postgres'
     POSTGRES_PASSWORD = 'postgres'
-    API_URL = 'http://0.0.0.0:5000'
-    REDIS_HOST = 'redis'
-    REDIS_PORT = '6379'
     JWT_SECRET_KEY = 'axcasdxaobisduba'
-    JSON_SORT_KEYS = False
     JWT_ACCESS_TOKEN_EXPIRES = 24 * 3600  # 1 day
     JWT_REFRESH_TOKEN_EXPIRES = 3 * 30 * 24 * 3600  # 3 months
     JWT_BLACKLIST_ENABLED = True
@@ -28,7 +25,7 @@ class Config(object):
     SWAGGER = {
         "specs": [
             {
-                "version": "2.0",
+                "version": "1.0",
                 "title": "Todo Apis",
                 "endpoint": "api",
                 "route": "/api/v1",
@@ -38,6 +35,31 @@ class Config(object):
 
     def __init__(self):
         load_dotenv(find_dotenv())
+
+    #     for k, v in os.environ.items():
+    #         if not k.startswith('TODO_'):
+    #             continue
+    #         key = k.replace('TODO_', '')
+    #         v = self._cast(v)
+
+    #         setattr(self, key, v)
+
+    # def _cast(self, v):
+    #     try:
+    #         v = float(v)
+    #         if v.is_integer():
+    #             v = int(v)
+    #     except ValueError:
+    #         if v == 'false':
+    #             v = False
+    #         elif v == 'true':
+    #             v = True
+    #         elif v == 'null' or v == 'None':
+    #             v = None
+    #         else:
+    #             pass
+
+    #     return v
 
     @property
     def postgres_url(self):
@@ -71,10 +93,6 @@ class Config(object):
             f':{self.POSTGRES_PORT}'
             f'/{self.POSTGRES_ADMIN_DB}'
         )
-
-    @property
-    def redis_url(self):
-        return f'redis://{self.REDIS_HOST}:{self.REDIS_PORT}'
 
     def to_dict(self):
         return dict(
